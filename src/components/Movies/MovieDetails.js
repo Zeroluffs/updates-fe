56db4771939f752078627f8d1a57f99177dd1844
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2021-06-30 11:39:10 -0500

    now its possible to add movies from details

--- src/components/Movies/MovieDetails.js
+++ src/components/Movies/MovieDetails.js
@@ -3,9 +3,10 @@ import { Typography } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import axios from "axios";
-
+import { AuthContext } from "../../context/auth";
 import "../../Styles/Movies/MovieDetails.css";
-
+import { addMovie } from "../../utils/helper.functions";
+import { useFoundItemState } from "../../utils/foundItem.hook";
 const api = axios.create({
   baseURL: `http://www.omdbapi.com/`,
 });
@@ -13,6 +14,9 @@ const MovieDetails = (props) => {
   const { REACT_APP_API_KEY3 } = process.env;
   const { movie } = props.location.movieProps;
   const [movieDetails, setMovieDetails] = useState([]);
+  const user = useContext(AuthContext);
+  const [found, setFound] = useState(false);
+  const isFound = useFoundItemState(user.user.id, movie.imdbID, "movies");
 
   useEffect(() => {
     api.get(`?apikey=${REACT_APP_API_KEY3}&i=${movie.imdbID}`).then((res) => {
@@ -29,7 +33,16 @@ const MovieDetails = (props) => {
           alt=""
           src={movie.Poster !== undefined ? movie.Poster : ""}
         ></img>
-        <Button variant="contained" color="primary" className="buttonAdd">
+        <Button
+          variant="contained"
+          color="primary"
+          className="buttonAdd"
+          disabled={found || isFound}
+          onClick={(e) => {
+            addMovie(movieDetails, user);
+            setFound(true);
+          }}
+        >
           Add To List
         </Button>
       </div>
